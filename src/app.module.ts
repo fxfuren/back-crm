e3e253d03874a62ed0f6ba26f3eee614265eabfd
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-21 00:56:46 +0300

    session management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
