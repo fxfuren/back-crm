45f79dda46794357e51d33cdd0663e3bd38b417a
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-21 01:12:27 +0300

    first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
       ignoreEnvFile: !IS_DEV_ENV,
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
