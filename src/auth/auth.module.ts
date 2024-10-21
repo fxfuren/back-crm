f5d9af63e8527f26dbc96d597b8da1510414812f
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-22 00:53:16 +0300

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserSevice } from 'src/user/user.service';
+import { UserService } from 'src/user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserSevice],
+  providers: [AuthService, UserService],
 })
 export class AuthModule {}
