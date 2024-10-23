799ff3d6c5d808330337f6d30ab91e7d4c7a46eb
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-23 17:19:50 +0300

    integration guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { UserRole } from 'prisma/__generated__';
+import { Authorization } from 'src/auth/decorators/auth.decorator';
+import { Authorized } from 'src/auth/decorators/authorized.decorator';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get('profile')
+  public async findProfile(@Authorized('id') userId: string) {
+    return this.userService.findById(userId);
+  }
+
+  @Authorization(UserRole.ADMIN)
+  @HttpCode(HttpStatus.OK)
+  @Get('by-id/:id')
+  public async findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
 }
