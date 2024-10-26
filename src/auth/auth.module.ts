6d163404a451e4d1f9383b26954ac754c60151c5
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-27 01:38:54 +0300

    integration mail-server

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
 import { getRecaptchaConfig } from 'src/config/recaptcha.config';
+import { MailService } from 'src/libs/mail/mail.service';
 import { UserService } from 'src/user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { EmailConfirmationModule } from './email-confirmation/email-confirmation.module';
 
 @Module({
   imports: [
@@ -13,8 +15,15 @@ import { AuthService } from './auth.service';
       useFactory: getRecaptchaConfig,
       inject: [ConfigService],
     }),
+    GoogleRecaptchaModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getRecaptchaConfig,
+      inject: [ConfigService],
+    }),
+    forwardRef(() => EmailConfirmationModule),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, MailService],
+  exports: [AuthService],
 })
 export class AuthModule {}
