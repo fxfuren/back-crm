5ee5e94d2fb9c4b679bf5d164b14024687f2f4b0
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-27 01:38:36 +0300

    imports in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { EmailConfirmationModule } from './auth/email-confirmation/email-confirmation.module';
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
+import { MailModule } from './libs/mail/mail.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
@@ -14,6 +16,8 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     AuthModule,
     UserModule,
+    MailModule,
+    EmailConfirmationModule,
   ],
 })
 export class AppModule {}
