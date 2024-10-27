8363fda1a8e9b91de61cbd64c1bd26e71bf26482
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-27 19:45:53 +0300

    genInvite

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { hash } from 'argon2';
+import { TokenType } from 'prisma/__generated__';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService {
@@ -9,6 +11,22 @@ export class UserService {
   public async allUser() {
     const user = await this.prismaService.user.findMany({});
     return user;
+  }
+
+  public async genInvite(adminEmail: string) {
+    const token = uuidv4();
+    const expiresIn = new Date(new Date().getTime() + 86400 * 1000);
+
+    const inviteToken = await this.prismaService.token.create({
+      data: {
+        email: adminEmail,
+        token,
+        expiresIn,
+        type: TokenType.INVITE,
+      },
+    });
+
+    return inviteToken;
   }
 
   public async findById(id: string) {
@@ -35,7 +53,6 @@ export class UserService {
     email: string,
     password: string,
     displayName: string,
-    picture: string,
     isVerified: boolean,
   ) {
     const user = await this.prismaService.user.create({
